0fa9f593d200067f1abde04d93f00a2120b29463
HamzaAliSolutionLoft <126680487+HamzaAliSolutionLoft@users.noreply.github.com>
2023-05-02 16:25:27 +0500

    first commit

---
+++ app/constants/Colors.js
@@ -0,0 +1,10 @@
+export default {
+  primaryColor: '#e45867',
+  secondaryColor: '#e6c1bc',
+  greenColor: '#1782F4',
+  lightGreenColor: '#cbe2fa',
+  grayColor: '#8F9BB3',
+  redColor: '#c4372c',
+  whiteColor: '#FFFFFF',
+  blackColor: '#000000',
+};
